39a957b5527b20d690ed033b820525256f23ddc1
Jan Phillip Schwietzer <schwietzer.jan@gmail.com>
2023-11-21 07:56:43 +0100

    Initial Commit

---
+++ SwiftMap/SwiftMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMapApp.swift
+//  SwiftMap
+//
+//  Created by Jan Schwietzer on 21.11.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
